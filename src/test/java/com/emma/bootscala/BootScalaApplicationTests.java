840d5e5551fcc7731fc18175d8eb2058a3fb2673
Emma <sh3956@columbia.edu>
2021-09-21 22:47:33 -0400

    Init: Java and Scala environment settings

---
+++ src/test/java/com/emma/bootscala/BootScalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma.bootscala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootScalaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
