4b37754127cde433864ad32df7a655a929f6442b
Emma <sh3956@columbia.edu>
2021-09-24 15:42:52 -0400

    Add: general response class

--- src/main/java/com/emma/bootscala/controller/MetaDatabaseController.java
+++ src/main/java/com/emma/bootscala/controller/MetaDatabaseController.java
@@ -2,11 +2,15 @@ package com.emma.bootscala.controller;
 
 import com.emma.bootscala.domain.MetaDatabase;
 import com.emma.bootscala.service.MetaDatabaseService;
+import com.emma.bootscala.utils.ResultVO;
+import com.emma.bootscala.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.transform.Result;
 
 @RestController
 @RequestMapping("/meta/database")
@@ -16,14 +20,14 @@ public class MetaDatabaseController {
     private MetaDatabaseService metaDatabaseService;
 
     @RequestMapping(value = "/", method= RequestMethod.POST)
-    public String save(@ModelAttribute MetaDatabase metaDatabase){
+    public ResultVO save(@ModelAttribute MetaDatabase metaDatabase){
         metaDatabaseService.save(metaDatabase);
-        return "success";
+        return ResultVOUtil.success();
     }
 
     @RequestMapping(value = "/", method= RequestMethod.GET)
-    public Iterable<MetaDatabase> query(){
-        return metaDatabaseService.query();
+    public ResultVO query(){
+        return ResultVOUtil.success(metaDatabaseService.query());
     }
 
 }
---
+++ src/main/java/com/emma/bootscala/utils/ResultVOUtil.java
@@ -0,0 +1,28 @@
+package com.emma.bootscala.utils;
+
+/**
+ * Boot return value util class
+ */
+public class ResultVOUtil {
+
+    public static ResultVO success(Object object){
+        ResultVO resultVO = new ResultVO();
+        resultVO.setData(object);
+        resultVO.setCode(0);
+        resultVO.setMsg("success");
+
+        return resultVO;
+    }
+
+    public static ResultVO success(){
+        return success(null);
+    }
+
+    public static ResultVO error(Integer code, String msg){
+        ResultVO resultVO = new ResultVO();
+        resultVO.setCode(code);
+        resultVO.setMsg(msg);
+
+        return resultVO;
+    }
+}
